69d356269a2fcce812e2de8ebeda86d35f740fdf
mike <miguel.rosa17@gmail.com>
2015-11-14 16:20:55 +0000

    Menus Completos - refactoring

--- Main.py
+++ Main.py
@@ -7,28 +7,35 @@ class mainWindow(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title="RC Car Controler", size=(700,500))
 
+        #Criar uma barra de menus e sub-menus
         menuBar = wx.MenuBar(0)
 
         #Sub-menu "Ficheiro"
         fileMenu = wx.Menu()
-        btnMenuSair = wx.MenuItem(fileMenu, wx.ID_ANY, "Sair") #Botao
+        btnMenuSair = wx.MenuItem(fileMenu, wx.ID_CLOSE, "Sair") #Botao
         fileMenu.AppendItem(btnMenuSair)
         menuBar.Append(fileMenu, "&Ficheiro")
 
         #sub-menu "Sobre"
         fileAbout = wx.Menu()
-        btnMenuSobre = wx.MenuItem(fileAbout, wx.ID_ANY, "Sobre") #Botao
+        btnMenuSobre = wx.MenuItem(fileAbout, wx.ID_HELP, "Sobre") #Botao
         fileAbout.AppendItem(btnMenuSobre)
-        btnWebsite = wx.MenuItem(fileAbout, wx.ID_ANY, "Pagina oficial") #Botao
-        fileAbout.AppendItem(btnWebsite)
+        btnMenuWebsite = wx.MenuItem(fileAbout, wx.ID_HOME, "Pagina oficial") #Botao
+        fileAbout.AppendItem(btnMenuWebsite)
         menuBar.Append(fileAbout, "&Sobre")
 
         #Adiciona a barra de menus
         self.SetMenuBar(menuBar)
 
+
+
+
+
+
         #adiciona os metodos aos botoes
         self.Bind(wx.EVT_MENU, self.btnMenuSair_onClick, btnMenuSair)
         self.Bind(wx.EVT_MENU, self.btnMenuSobre_onClick, btnMenuSobre)
+        self.Bind(wx.EVT_MENU, self.btnMenuWebSite_onClick, btnMenuWebsite)
 
         #mostra a janela
         self.Show(True)
@@ -42,6 +49,9 @@ class mainWindow(wx.Frame):
         dialog = wx.MessageDialog(self, "Programa para controlar RC Car\n \nDesenvolvido por: Miguel Rosa\n \n \n \nTodos os direitos reservados\n \n \nMértola 2016", "RC Car Controller - Sobre")
         dialog.ShowModal()#Mostra a caixa de dialogo
         dialog.Destroy() #destroi a caixa de dialogo
+
+    def btnMenuWebSite_onClick(self, event):
+        methods.openWebPageFromGitHub()
 
 
 
--- methods.py
+++ methods.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+import webbrowser
 
 def helloWorld():
     print "HELLO MIKE"
+
+
+def openWebPageFromGitHub():
+    linkToPage = "https://github.com/imiguel/Arduino_RC_Car/wiki"
+    webbrowser.open_new(linkToPage)
