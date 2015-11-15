816d714f6fa42ca27ec08c9fe827aa48f46446b9
mike <miguel.rosa17@gmail.com>
2015-11-15 16:07:30 +0000

    Menu formato final

--- app_pc/menuTkinter.py
+++ app_pc/menuTkinter.py
@@ -37,14 +37,17 @@ class mainWindow(wx.Frame):
         labelTextoMovimento = wx.StaticText(self, label="Movimento", pos=(36, 50), style=wx.ALIGN_CENTER)
         labelTextoOpcoes = wx.StaticText(self, label="OPÇÕES", pos=(550, 50), style=wx.ALIGN_CENTER)
         labelLigarLuzes = wx.StaticText(self, label="Ligar as Luzes (frente)", pos=(400, 120), style=wx.ALIGN_CENTER)
+        labelLigarPiscas = wx.StaticText(self, label="Ligar os piscas", pos=(400, 190), style=wx.ALIGN_CENTER)
 
         #Definicoes das fontes das labels
         fontToLabelTextoMovimento = wx.Font(26, wx.DEFAULT, wx.NORMAL, wx.BOLD)
         fontToLabelTextoOpcoes = wx.Font(26, wx.DEFAULT, wx.NORMAL, wx.BOLD)
         fontToLabelLigarLuzes = wx.Font(14, wx.DEFAULT, wx.NORMAL, wx.BOLD)
+        fontToLabelLigarPiscas = wx.Font(14, wx.DEFAULT, wx.NORMAL, wx.BOLD)
         labelTextoMovimento.SetFont(fontToLabelTextoMovimento)
         labelTextoOpcoes.SetFont(fontToLabelTextoOpcoes)
         labelLigarLuzes.SetFont(fontToLabelLigarLuzes)
+        labelLigarPiscas.SetFont(fontToLabelLigarPiscas)
 
         ##ESTES 4 botoes ESTAO CERTOS!!!
         buttonMoveUP = wx.Button(self, pos=(85, 100), size=(45, 45), id=wx.ID_ANY, label="UP")
@@ -53,6 +56,9 @@ class mainWindow(wx.Frame):
         buttonMoveUP = wx.Button(self, pos=(135, 150), size=(45, 45), id=wx.ID_ANY, label="RIGHT")
         buttonCarLightsOn = wx.Button(self, pos=(430, 140), size=(30, 30), id=wx.ID_ANY, label="ON") #FALTA A ACCAO PARA O BOTAO
         buttonCarLightsOff = wx.Button(self, pos=(470, 140), size=(30, 30), id=wx.ID_ANY, label="OFF") #FALTA A ACCAO PARA O BOTAO
+        buttonCarPiscasEsq = wx.Button(self, pos=(430, 210), size=(50, 30), id=wx.ID_ANY, label="Esquerda") #FALTA A ACCAO PARA O BOTAO
+        buttonCarPiscasTodos = wx.Button(self, pos=(490, 210), size=(40, 30), id=wx.ID_ANY, label="Todos") #FALTA A ACCAO PARA O BOTAO
+        buttonCarPiscasDrt = wx.Button(self, pos=(540, 210), size=(50, 30), id=wx.ID_ANY, label="Direita") #FALTA A ACCAO PARA O BOTAO
 
 
         #adiciona os metodos aos botoes
